808fccfe9963576b7fedca14a6a4894c9ab08419
Federico Iosue <federico.iosue@gmail.com>
2017-02-13 00:26:11 +0100

    Updated Piwik library to 1.0.2

--- analitica-lib/src/piwik/java/android/feio/it/analitica/PiwikAnalyticsHelper.java
+++ analitica-lib/src/piwik/java/android/feio/it/analitica/PiwikAnalyticsHelper.java
@@ -7,6 +7,7 @@ import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
 import org.piwik.sdk.Piwik;
+import org.piwik.sdk.TrackHelper;
 import org.piwik.sdk.Tracker;
 
 import java.net.MalformedURLException;
@@ -27,7 +28,7 @@ public class PiwikAnalyticsHelper extends AnalyticsAbstractHelper {
 				tracker = Piwik.getInstance(application).newTracker(analyticsUrl, 1);
 				tracker.setUserId(Settings.Secure.getString(application.getContentResolver(), Settings.Secure
 						.ANDROID_ID));
-				tracker.trackAppDownload();
+				TrackHelper.track().download().with(tracker);
 			} catch (MalformedURLException e) {
 				Log.e(TAG, "Malformed url to get analytics tracker", e);
 			}
@@ -42,36 +43,29 @@ public class PiwikAnalyticsHelper extends AnalyticsAbstractHelper {
 
 	@Override
 	public void trackScreenView(String screenName) {
-		if (checkInit()) {
-			tracker.trackScreenView(screenName);
+		if (enabled) {
+			TrackHelper.track().screen(screenName).with(tracker);
 		}
 	}
 
 
 	@Override
 	public void trackEvent(CATEGORIES category, String action) {
-		if (checkInit()) {
-			tracker.trackEvent(category.name(), action);
+		if (enabled) {
+			TrackHelper.track().event(category.name(), action).with(tracker);
 		}
 	}
 
 
 	@Override
 	public void trackActionFromResourceId(Activity activity, int resourceId) {
-		if (checkInit()) {
+		if (enabled) {
 			try {
-				tracker.trackEvent(CATEGORIES.ACTION.name(), activity.getResources().getResourceEntryName(resourceId));
+				TrackHelper.track().event(CATEGORIES.ACTION.name(), activity.getResources().getResourceEntryName
+						(resourceId)).with(tracker);
 			} catch (Resources.NotFoundException e) {
 				Log.w(TAG, "No resource name found for request id");
 			}
 		}
-	}
-
-
-	private static boolean checkInit() {
-		if (enabled && tracker == null) {
-			throw new NullPointerException("Call GoogleAnalyticsHelper.init() before using analytics tracker");
-		}
-		return enabled;
 	}
 }
